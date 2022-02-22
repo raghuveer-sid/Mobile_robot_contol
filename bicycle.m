c560ef2025ffe472c11b74d695647c6087cbd15e
Sid <62798224+raghuveer-sid@users.noreply.github.com>
2022-02-22 21:18:20 +0100

    Add files via upload

---
+++ bicycle.m
@@ -0,0 +1,41 @@
+clear
+
+
+% reference trajectory tuning
+a = 4;
+w = 0.1;
+
+% initial position
+x0 = a;
+y0 = 0;
+theta0 = pi/2;
+Kp = 7 ; % 1-10
+d = 0.01; % small
+Kx = 30 ;
+Ky = 30 ;
+K_theta = 1 ;
+
+t = sim('bicycle_sim11',6*pi/w);
+
+close all
+figure(1)
+hold on
+ax = gca;
+ax.ColorOrderIndex = 1;
+plot(xy_ref(:,1), xy_ref(:,2), xy(:,1), xy(:,2))
+legend('Desired', 'Actual')
+axis square
+
+ figure(2)
+subplot(2,1,1)
+plot(t, error)
+legend('x-error', 'y-error')
+xlabel('Time [s]')
+ylabel('Error [m]')
+
+
+subplot(2,1,2)
+plot(t, 180/pi*omega_beta)
+legend('velocity [deg/s]', 'orientation [deg]')
+xlabel('Time [s]')
+ylabel('Wheel controls')
---
+++ diff_drive.m
@@ -0,0 +1,41 @@
+clear
+
+% reference trajectory tuning
+a = 4;
+w = 0.1;
+
+% initial position
+x0 = a;
+y0 = 0;
+theta0 = pi/2;
+d = 0.075;%10;
+Kx = 15;
+Ky = 150;
+K_theta = 0.001;
+Kp = 1;
+
+% do 3 turns
+t = sim('diff_drive_sim',6*pi/w);
+
+close all
+figure(1)
+hold on
+ax = gca;
+ax.ColorOrderIndex = 1;
+plot(xy_ref(:,1), xy_ref(:,2), xy(:,1), xy(:,2))
+legend('Desired', 'Actual')
+axis square
+
+figure(2)
+subplot(2,1,1)
+plot(t, error)
+legend('x-error', 'y-error')
+xlabel('Time [s]')
+ylabel('Error [m]')
+
+
+subplot(2,1,2)
+plot(t, phi_dot)
+legend('left', 'right')
+xlabel('Time [s]')
+ylabel('Wheel velocity [rad/s]')
